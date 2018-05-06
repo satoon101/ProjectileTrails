fbf06ab5a4281955b94d1ecfb7fcc3190d3e4cf1
satoon101 <satoon101@gmail.com>
2018-05-06 15:20:10 -0400

    Added dust/sparks trails.

---
+++ addons/source-python/plugins/projectile_trails/effects/dust.py
@@ -0,0 +1,34 @@
+# ../projectile_trails/effects/dust.py
+
+"""Provides a dust trail effect."""
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Source.Python
+from effects.base import TempEntity
+
+# Plugin
+from .base import BaseEffect
+
+
+# =============================================================================
+# >> ALL DECLARATION
+# =============================================================================
+__all__ = (
+    'Dust',
+)
+
+
+# =============================================================================
+# >> CLASSES
+# =============================================================================
+class Dust(BaseEffect):
+    """Dust trail effect."""
+
+    def update_trail(self):
+        """Create the smoke trail for the given entity."""
+        entity = TempEntity('Dust')
+        entity.origin = self.entity.origin
+        entity.size = 20
+        entity.create()
---
+++ addons/source-python/plugins/projectile_trails/effects/sparks.py
@@ -0,0 +1,38 @@
+# ../projectile_trails/effects/sparks.py
+
+"""Provides a sparks trail effect."""
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Source.Python
+from effects.base import TempEntity
+
+# Plugin
+from .base import BaseEffect
+
+
+# =============================================================================
+# >> ALL DECLARATION
+# =============================================================================
+__all__ = (
+    'Sparks',
+)
+
+
+# =============================================================================
+# >> CLASSES
+# =============================================================================
+class Sparks(BaseEffect):
+    """Sparks trail effect."""
+
+    def update_trail(self):
+        """Create the sparks trail for the given entity."""
+        entity = TempEntity('Sparks')
+        entity.origin = self.entity.origin
+        direction = self.entity.base_velocity
+        direction.negate()
+        entity.direction = direction
+        entity.trail_length = 3
+        entity.magnitude = 1
+        entity.create()
