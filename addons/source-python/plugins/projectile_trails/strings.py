33eeaf7885ca321cf89b0f0c59b70afa63b0b237
satoon101 <satoon101@gmail.com>
2025-06-19 18:37:02 -0400

    Minor changes.

--- addons/source-python/plugins/projectile_trails/strings.py
+++ addons/source-python/plugins/projectile_trails/strings.py
@@ -1,6 +1,6 @@
 # ../projectile_trails/strings.py
 
-"""Contains all translation variables for the base plugin."""
+"""Contains all translation variables for the plugin."""
 
 # =============================================================================
 # >> IMPORTS
